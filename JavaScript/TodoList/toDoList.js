3d8cd00ff271c2a55507d8ee48f44940ffc2d265
Raul Menjivar <raul.menjivar4@gmail.com>
2019-04-26 00:37:36 -0500

    twentyfour

--- JavaScript/TodoList/toDoList.js
+++ JavaScript/TodoList/toDoList.js
@@ -4,12 +4,47 @@ var input = prompt("What would you like to do?");
 
 while(input !== "quit") {
     if (input === "list") {
-        console.log(todos);
-    } 
+        listTodos();
+    }
     else if (input === "new") {
-        var newTodo = prompt("Enter new todo");
-        todos.push(newTodo);
+        addTodo();
+    }
+    else if (input === "delete") {
+        deleteTodo();
     }
     input = prompt("What would you like to do?");
 }
 console.log("OK, YOU QUIT THE APP");
+
+
+
+
+
+//-----FUNCTIONS-------
+
+
+function listTodos() {
+    console.log("**********")
+    todos.forEach(function(todo, index) {
+        console.log(index + ": " + todo);
+    });
+    console.log("**********");
+    };
+
+
+function addTodo() {
+    //ask for new todo
+    var newTodo = prompt("Enter new todo");
+    //add to todos array
+    todos.push(newTodo);
+    console.log("Added Todo");
+}
+
+function deleteTodo() {
+    //ask for index of todo to be deleted
+    var index = prompt("Enter index of todo to delete");
+    //delete that todo
+    //splice()
+    todos.splice(index,1);
+    console.log("Deleted Todo");
+}
