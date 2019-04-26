43ba6ca9d5eec6e85a04c5b8b73f709f695db3da
Raul Menjivar <raul.menjivar4@gmail.com>
2019-04-26 00:55:59 -0500

    twentyfive

---
+++ JavaScript/Arrays/exercise2.js
@@ -0,0 +1,23 @@
+//Write a function printReverse() that takes an array as an argument and prints out the elements in the array in reverse order (don't actually reverse the array itself)
+
+function printReverse() {
+
+}
+
+//Write a function isUniform() which takes an array as an argument and returns true if all elements in the array are identical
+
+function isUniform() {
+
+}
+
+//Write a function sumArray() that accepts an array of numbers and returns the sum of all numbers in the array
+
+function sumArray() {
+
+}
+
+//Write a function max() that accepts an array of numbers and returns the maximum number in the array
+
+function max() {
+    
+}
