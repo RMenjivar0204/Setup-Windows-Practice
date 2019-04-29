d98a74012d6f5ad5f82725ca2baf9701f2ff75cf
Raul Menjivar <raul.menjivar4@gmail.com>
2019-04-29 14:48:13 -0500

    twentyfive

---
+++ DOM/exercise1.js
@@ -0,0 +1,9 @@
+//Come up with 4 different ways to select the first <p> tag
+
+var first = document.querySelector("#first");
+
+var second = document.getElementById("first");
+
+var third = document.getElementsByClassName("special")[0];
+
+var fourth =document.querySelectorAll(".special")[0];
--- JavaScript/Objects/exercise1.js
+++ JavaScript/Objects/exercise1.js
@@ -1,5 +1,31 @@
-var person = {
-    name: "Raul" ,
-    age: 26 ,
-    city: "Houston" ,
-};
+var movieDB = [
+    {
+        title: "Pizza Boy",
+        rating: 2.5 ,
+        watched: true
+    },
+    {
+        title: "Shark in the Park 2" ,
+        rating: 3.0 ,
+        watched: false
+    },
+    {
+        title: "Groovy Movies: A Disco Movie Documentary" ,
+        rating: 4.7 ,
+        watched: true
+    }
+];
+
+
+movieDB.forEach(function(movie) {
+    var result = "You have ";
+    if (movie.watched) {
+        result += "watched ";
+    }
+    else {
+        result += "not seen";
+    }
+    result += "\"" + movie.title + "\" - ";
+    result += movie.rating + " stars";
+    console.log(result)
+})
