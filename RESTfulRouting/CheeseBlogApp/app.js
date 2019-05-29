81d265b4cbae1c3dc3dd9bf8f4c063338509c0f0
Raul Menjivar <raul.menjivar4@gmail.com>
2019-05-28 22:16:02 -0500

    cheese

---
+++ RESTfulRouting/CheeseBlogApp/app.js
@@ -0,0 +1,79 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var expressSanitizer = require("express-sanitizer");
+var methodOverride = require("method-override");
+var mongoose = require("mongoose");
+
+
+
+// APP CONFIG
+
+mongoose.connect("mongodb://localhost/cheese_blog_app", { useNewUrlParser: true});
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
+
+
+// MONGOOSE/MODEL CONFIG
+
+var cheeseSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: { type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", cheeseSchema);
+
+
+//RESTFUL ROUTES
+
+app.get("/", function(req, res){
+    res.redirect("/blogs");
+})
+
+// INDEX ROUTE 
+app.get("/blogs", function(req, res){
+    res.render("index");
+})
+
+// NEW ROUTE
+app.get("/blogs/new", function(req, res){
+
+});
+
+// CREATE ROUTE 
+app.post("/blogs", function(req, res){
+
+});
+
+// SHOW ROUTE
+app.get("/blogs/:id", function(req, res){
+
+});
+
+// EDIT ROUTE
+app.get("/blogs/:id/edit", function(req, res){
+
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+
+});
+
+// DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+    
+});
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("Listening on Port 3000");
+})
--- RESTfulRouting/RESTfulBlogApp/app.js
+++ RESTfulRouting/RESTfulBlogApp/app.js
@@ -2,6 +2,8 @@ var express     = require("express");
 var app         = express();
 var bodyParser  = require("body-parser");
 var mongoose    = require("mongoose");
+var methodOverride  = require("method-override");
+var expressSanitizer = require("express-sanitizer");
 
 
 //APP CONFIG
@@ -10,7 +12,8 @@ mongoose.connect("mongodb://localhost/restful_blog_app", { useNewUrlParser: true
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(methodOverride("_method"));
+app.use(expressSanitizer());
 
 
 // MONGOOSE/MODEL CONFIG
@@ -51,6 +54,10 @@ app.get("/blogs/new", function(req, res){
 // CREATE ROUTE
 app.post("/blogs", function(req, res){
     // create blog
+    console.log(req.body);
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    console.log("============");
+    console.log(req.body);
     Blog.create(req.body.blog, function(err, newBlog){
         if(err){
             res.render("new");
@@ -60,6 +67,53 @@ app.post("/blogs", function(req, res){
         }
     })
 });
+
+// SHOW ROUTE
+app.get("/blogs/:id", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("show", {blog: foundBlog});
+        }
+    })
+});
+
+// EDIT ROUTE 
+app.get("/blogs/:id/edit", function(req, res){
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.render("edit", {blog: foundBlog});
+        }
+    })
+});
+
+// UPDATE ROUTE
+app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs/" + req.params.id);
+        }
+    })
+})
+
+// DELETE ROUTE
+app.delete("/blogs/:id", function(req, res){
+    // destroy blog
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    })
+    // redirect somewhere
+})
 
 
 app.listen(3000, function(){
